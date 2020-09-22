25659d770ef8d68493f3fcd2376e16a5bb69d1ea
shachitha <shachithakv.akl@gmail.com>
2020-09-22 23:11:17 +0530

    Updates

---
+++ precache-manifest.089593069690311bda86cb73ac3f670a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1082a87a68910e2f7fc2f20c867a3623",
+    "url": "/travel/index.html"
+  },
+  {
+    "revision": "15d07d47ee0cefc1b685",
+    "url": "/travel/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0d1eb55963fed2465c99",
+    "url": "/travel/static/js/2.7054c213.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/travel/static/js/2.7054c213.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15d07d47ee0cefc1b685",
+    "url": "/travel/static/js/main.36d96500.chunk.js"
+  },
+  {
+    "revision": "5734ad54b26bd5ccfde1",
+    "url": "/travel/static/js/runtime-main.16f54be6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/travel/static/media/logo.5d5d9eef.svg"
+  }
+]);
